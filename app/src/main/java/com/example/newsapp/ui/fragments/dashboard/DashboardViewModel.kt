16c2e30bb8870c1c86918db029712b0f50c1c7f2
Gagan Batra <g.batra24@gmail.com>
2023-08-08 14:08:26 +0530

    Introduced coroutines for retrofit calls

--- app/src/main/java/com/example/newsapp/ui/fragments/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/newsapp/ui/fragments/dashboard/DashboardViewModel.kt
@@ -2,10 +2,12 @@ package com.example.newsapp.ui.fragments.dashboard
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.newsapp.data.api.NetworkService
 import com.example.newsapp.data.api.RetrofitClient
 import com.example.newsapp.data.models.ListingsResponse
 import com.example.newsapp.utils.Constants
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -19,20 +21,22 @@ class DashboardViewModel : ViewModel() {
         var client = RetrofitClient.getInstance()
         var service = client.create(NetworkService::class.java)
 
-        service.getNewsBySearch(Constants.API_KEY, query, "popularity")
-            .enqueue(object : Callback<ListingsResponse> {
-                override fun onResponse(
-                    call: Call<ListingsResponse>,
-                    response: Response<ListingsResponse>
-                ) {
-                    if (response.isSuccessful) {
-                        searchResponseLiveData.value = response.body()
+        viewModelScope.launch {
+            service.getNewsBySearch(Constants.API_KEY, query, "popularity")
+                .enqueue(object : Callback<ListingsResponse> {
+                    override fun onResponse(
+                        call: Call<ListingsResponse>,
+                        response: Response<ListingsResponse>
+                    ) {
+                        if (response.isSuccessful) {
+                            searchResponseLiveData.value = response.body()
+                        }
                     }
-                }
 
-                override fun onFailure(call: Call<ListingsResponse>, t: Throwable) {
-                    searchErrorLiveData.value = t.message
-                }
-            })
+                    override fun onFailure(call: Call<ListingsResponse>, t: Throwable) {
+                        searchErrorLiveData.value = t.message
+                    }
+                })
+        }
     }
 }
--- app/src/main/java/com/example/newsapp/ui/fragments/listings/ListingsViewModel.kt
+++ app/src/main/java/com/example/newsapp/ui/fragments/listings/ListingsViewModel.kt
@@ -2,10 +2,12 @@ package com.example.newsapp.ui.fragments.listings
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.newsapp.data.api.NetworkService
 import com.example.newsapp.data.api.RetrofitClient
 import com.example.newsapp.data.models.ListingsResponse
 import com.example.newsapp.utils.Constants
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -19,22 +21,24 @@ class ListingsViewModel : ViewModel() {
         val client = RetrofitClient.getInstance()
         val service = client.create(NetworkService::class.java)
 
-        service.getTopHeadlines(Constants.API_KEY, "in", 1, 10)
-            .enqueue(object : Callback<ListingsResponse> {
-                override fun onResponse(
-                    call: Call<ListingsResponse>,
-                    response: Response<ListingsResponse>
-                ) {
-                    if (response.isSuccessful) {
-                        listingsResponseLiveData.value = response.body()
+        viewModelScope.launch {
+            service.getTopHeadlines(Constants.API_KEY, "in", 1, 10)
+                .enqueue(object : Callback<ListingsResponse> {
+                    override fun onResponse(
+                        call: Call<ListingsResponse>,
+                        response: Response<ListingsResponse>
+                    ) {
+                        if (response.isSuccessful) {
+                            listingsResponseLiveData.value = response.body()
+                        }
                     }
-                }
 
-                override fun onFailure(call: Call<ListingsResponse>, t: Throwable) {
-                    listingsErrorLiveData.value = t.message
-                }
+                    override fun onFailure(call: Call<ListingsResponse>, t: Throwable) {
+                        listingsErrorLiveData.value = t.message
+                    }
 
-            })
+                })
+        }
 
     }
 
